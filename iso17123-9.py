da38b4e1396a66718daa7b5a3fb92aee1d1f7838
Colin Wallimann <colin.wallimann@protonmail.com>
2024-04-19 11:53:50 +0200

    Added interactive inputs if no args given

--- iso17123-9.py
+++ iso17123-9.py
@@ -21,12 +21,12 @@ if __name__ == '__main__':
     parser.add_argument('-alpha', type=float, default=0.05, help='confidence interval (default: 0.05)')
 
     simple_group = parser.add_argument_group('Simplified test procedure')
-    simple_group.add_argument('-u_t', type=float, required='-full' not in sys.argv, help='uncertainty quantity u_T for a targets center')
+    simple_group.add_argument('-u_t', type=float, help='uncertainty quantity u_T for a targets center')
 
     full_group = parser.add_argument_group('Full test procedure')
-    full_group.add_argument('-case', required='-full' in sys.argv, help='Which case for a target uncertainty should be used (see 8.5.1 in the ISO document)')
-    full_group.add_argument('-u_ms', type=float, required=any(arg.lower() == 'a' for arg in sys.argv), help='Manufacturer specified target center uncertainty (Case A)')
-    full_group.add_argument('-u_p', type=float, required=any(arg.lower() == 'b' for arg in sys.argv), help='derived target center uncertainty from other sources (Case B)')
+    full_group.add_argument('-case', help='Which case for a target uncertainty should be used (see 8.5.1 in the ISO document)')
+    full_group.add_argument('-u_ms', type=float, help='Manufacturer specified target center uncertainty (Case A)')
+    full_group.add_argument('-u_p', type=float, help='derived target center uncertainty from other sources (Case B)')
 
     args = parser.parse_args()
     print(sys.argv)
@@ -42,7 +42,7 @@ if __name__ == '__main__':
         print(f'Supported manufacturers are: {newline}{newline.join(supported_formats)}')
         sys.exit()
 
-    if args.full and args.case.lower() not in 'abc':
+    if args.case and (args.full and args.case.lower() not in 'abc'):
         print('Invalid case! Must be A, B or C, see 8.5.1 in the ISO document')
         sys.exit()
 
@@ -55,9 +55,25 @@ if __name__ == '__main__':
 
     measurements = read.read_path(args.target_path, args.manufacturer, args.ff, args.full)
 
+    # Interactive data collection:
+    if args.full:
+        if not args.case:
+            args.case = input('Case for testing the uncertainty of a targets center\n> ')
+            if args.case.lower() not in 'abc':
+                print('Invalid case! Must be A, B or C, see 8.5.1 in the ISO document')
+                sys.exit()
+        if args.case.lower() == 'a' and not args.u_ms:
+            args.u_ms = float(input('Target center uncertainty (u_ms) as specified by the manufacturer\n> '))
+        if args.case.lower() == 'b' and not args.u_p:
+            args.u_p = float(input('3D point uncertainty (u_P) derived from other sources\n> '))
+    else:  # simplified
+        if not args.u_t:
+            args.u_t = float(input('Target center uncertainty (u_T) to compare against\n> '))
+
+
     if args.full:
         test = procedures.Full(measurements, test_case=args.case, u_ms=args.u_ms, u_p=args.u_p, alpha=args.alpha)
         print_results.full(test)
-    else:
+    else:  # simplified
         test = procedures.Simplified(measurements, u_T=args.u_t, alpha=args.alpha)
         print_results.simplified(test)
